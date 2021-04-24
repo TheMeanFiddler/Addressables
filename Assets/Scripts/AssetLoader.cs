c168fac7ed37892825f61caf81eea7ab8cc4b61c
TheMeanFiddler <jason_basin@hotmail.co.uk>
2021-04-24 12:38:12 +0100

    added all
    added all

---
+++ Assets/Scripts/AssetLoader.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
+
+public class AssetLoader : MonoBehaviour
+{
+    public AssetReference objectToLoad;
+    private GameObject goObject;
+    Text txtTrace;
+
+    private void Start()
+    {
+
+                txtTrace = GameObject.Find("txtTrace").GetComponent<Text>();
+
+    }
+    public void LoadAssets()
+    {
+        txtTrace.text=("Button click");
+        Debug.Log(objectToLoad.RuntimeKey);
+        Addressables.LoadAssetAsync<GameObject>(objectToLoad).Completed += ObjectLoadDone;
+    }
+
+    private void ObjectLoadDone(AsyncOperationHandle<GameObject> obj)
+    {
+        txtTrace.text += "\nLoadObjDone";
+        if (obj.Status == AsyncOperationStatus.Succeeded)
+        {
+            GameObject loadedObject = obj.Result;
+            Debug.Log("Successfully loaded object ");
+            goObject = Instantiate(loadedObject);
+            goObject.name = "NewObj";
+            goObject.transform.position = Vector3.zero;
+        }
+        }
+}
